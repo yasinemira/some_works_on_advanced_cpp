39a5a5e89badef887901bb9a746a7150b35c2ff4
M. Galib Uludag <m231@tnctr.com>
2023-07-02 20:09:57 +0000

    clean type_traits

--- src/common/calculations.h
+++ src/common/calculations.h
@@ -6,13 +6,17 @@
 #include "ShapeManager.h"
 
 namespace Common {
+    namespace Details{
+        template <typename T>
+        using remove_cvref_t = std::remove_reference_t<std::remove_cv_t<T>>;
+    }
 
 namespace Calculations {
 
 template <typename... Shapes>
 const double findGreatestSurfaceArea(Shapes&&... shapes)
 {   
-    static_assert(std::conjunction_v<std::is_base_of<ShapeManager, std::remove_reference_t<std::remove_cv_t<Shapes>>>...>, "Class doesn't inherit from ShapeManager!");
+    static_assert(std::conjunction_v<std::is_base_of<ShapeManager, Common::Details::remove_cvref_t<Shapes>>...>, "Class doesn't inherit from ShapeManager!");
     const auto maxSurfaceArea = std::max({shapes.calcArea()...});
     std::cout << "Max surface area is: " << maxSurfaceArea << "\n";
     return maxSurfaceArea;
